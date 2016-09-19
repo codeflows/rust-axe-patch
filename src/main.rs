4c7bf5b504a6da0be6ab5208fe6c6c6dd3c9deaa
Jari Aarniala <jari.aarniala@gmail.com>
2016-09-19 22:57:19 +0300

    Validate Axe FX patch header

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,23 @@ fn main() {
     }
 }
 
-fn read_syx(file: String) {
-    let mut file = File::open(file).unwrap();
+fn read_syx(file_name: String) {
+    println!("Reading {} ...", file_name);
+
+    let mut file = File::open(file_name).unwrap();
     let mut buf = [0u8; 12];
-    file.read(&mut buf);
+    file.read(&mut buf).unwrap();
+
+    if !validate(&buf) {
+        println!("This does not look like a Axe FX patch file.");
+        std::process::exit(-1);
+    }
+
     for b in buf.iter() {
         print!("{:01$X} ", b, 2);
     }
+}
+
+fn validate(buf: &[u8]) -> bool {
+    buf.len() > 4 && buf[0] == 0xF0 && buf[1] == 0x00 && buf[2] == 0x01 && buf[3] == 0x74
 }
