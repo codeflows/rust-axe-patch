3805b36c5173981632dd35f09e6c9423a9a8f37d
Jari Aarniala <jari.aarniala@gmail.com>
2016-09-19 22:33:46 +0300

    Print out syx in hex

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,21 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    let fileArg = env::args().nth(1);
-    match fileArg {
-        Some(file) => println!("Juuh! {:?}", file),
+    let file_arg = env::args().nth(1);
+    match file_arg {
+        Some(file) => read_syx(file),
         None => {
             println!("Usage: cargo run file.syx");
             std::process::exit(-1);
         }
+    }
+}
+
+fn read_syx(file: String) {
+    let mut file = File::open(file).unwrap();
+    let mut buf = [0u8; 12];
+    file.read(&mut buf);
+    for b in buf.iter() {
+        print!("{:X} ", b);
     }
 }
