8f80b683e97b02328a184b0061fc11aee91a3c63
Jari Aarniala <jari.aarniala@gmail.com>
2016-09-28 19:54:50 +0300

    Find first sysex message start / end

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,43 @@ fn main() {
         std::process::exit(-1);
     }
     for file in env::args().skip(1) {
-        read_syx(file)
+        read_sysex_file(file)
     }
 }
 
-fn read_syx(file_name: String) {
+fn read_sysex_file(file_name: String) {
     println!("Reading {} ...", file_name);
 
     let mut file = File::open(file_name).unwrap();
     let mut buf: Vec<u8> = Vec::new();
     file.read_to_end(&mut buf).unwrap();
 
+    parse_sysex_data(&buf);
+}
+
+const SYSEX_MESSAGE_START_BYTE: u8 = 0xf0;
+const SYSEX_MESSAGE_END_BYTE: u8 = 0xf7;
+
+fn parse_sysex_data(data: &[u8]) {
+    let start = data[0];
+    if start != SYSEX_MESSAGE_START_BYTE {
+        panic!("No start byte found");
+    }
+    let end = find_sysex_message_end(data).unwrap();
+    let message = &data[0..end+1];
+    read_syx(message);
+}
+
+fn find_sysex_message_end(data: &[u8]) -> Option<usize> {
+    for (index, byte) in data.iter().enumerate() {
+        if *byte == SYSEX_MESSAGE_END_BYTE {
+            return Some(index);
+        }
+    }
+    return None;
+}
+
+fn read_syx(buf: &[u8]) {
     let model = axe_model_name(buf[4]);
     println!("Axe FX model: {}", model);
 
@@ -29,10 +55,7 @@ fn read_syx(file_name: String) {
 
     if !validate_header(&buf) {
         println!("This does not look like a Axe FX patch file.");
-        for b in buf.iter() {
-            print!("{:01$X} ", b, 2);
-        }
-        println!("");
+        print_bytes(buf);
         std::process::exit(-1);
     }
 
@@ -45,8 +68,6 @@ fn read_syx(file_name: String) {
 }
 
 fn validate_header(buf: &[u8]) -> bool {
-    // sysex start
-    buf[0] == 0xF0 &&
     // "Manufacturer sysex ID byte 0. As of firmware 8.02 this is always 00."
     buf[1] == 0x00 &&
     // "Manufacturer sysex ID byte 1. As of firmware 10.02, this is always 01 (in previous firmware versions this was 00).""
@@ -60,9 +81,7 @@ fn validate_header(buf: &[u8]) -> bool {
         buf[5] == 0x7a ||
         // MIDI_PATCH_DUMP? standard and ultra patches?
         buf[5] == 0x04
-    ) &&
-    // TODO assuming header length is 12 bytes here, should search for end instead
-    buf[11] == 0xf7
+    )
 }
 
 fn axe_model_name(code: u8) -> &'static str {
@@ -76,4 +95,11 @@ fn axe_model_name(code: u8) -> &'static str {
         0x08 => "AX8",
         _    => "Unknown"
     }
+}
+
+fn print_bytes(buf: &[u8]) {
+    for b in buf.iter() {
+        print!("{:01$X} ", b, 2);
+    }
+    println!("");
 }
